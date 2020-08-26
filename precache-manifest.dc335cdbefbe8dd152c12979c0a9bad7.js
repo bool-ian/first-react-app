525090bcfa4b9a3bd23282ce6ff4963708b24828
Ian Gamble <iangamble@Ians-MacBook-Pro.local>
2020-08-26 17:47:59 -0400

    Updates

---
+++ precache-manifest.dc335cdbefbe8dd152c12979c0a9bad7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4131061d4e435ee7d5f40382687dda24",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "4cf3173f4b0a2a48a8e5",
+    "url": "/first-react-app/static/css/2.dd0b1393.chunk.css"
+  },
+  {
+    "revision": "04a8dd1afa860109076f",
+    "url": "/first-react-app/static/css/main.d193393e.chunk.css"
+  },
+  {
+    "revision": "4cf3173f4b0a2a48a8e5",
+    "url": "/first-react-app/static/js/2.c94fbc4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first-react-app/static/js/2.c94fbc4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a8dd1afa860109076f",
+    "url": "/first-react-app/static/js/main.37923e80.chunk.js"
+  },
+  {
+    "revision": "961a7c2183c95fecb301",
+    "url": "/first-react-app/static/js/runtime-main.c885a8ab.js"
+  },
+  {
+    "revision": "73cd4a03f8aefb603474bdd26688eaa2",
+    "url": "/first-react-app/static/media/deer.73cd4a03.bin"
+  },
+  {
+    "revision": "4bd43a353d2f37a38693d474986243b5",
+    "url": "/first-react-app/static/media/dragging.4bd43a35.bin"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2bf71c7dd95c85ac29afdc60c35dc8b2",
+    "url": "/first-react-app/static/media/shooting.2bf71c7d.bin"
+  },
+  {
+    "revision": "a6c1bda237179d92b895e058cb65af31",
+    "url": "/first-react-app/static/media/tree.a6c1bda2.bin"
+  },
+  {
+    "revision": "450907bd30a8435b2ee20ea41b256408",
+    "url": "/first-react-app/static/media/turkey.450907bd.bin"
+  },
+  {
+    "revision": "023a46b71cbec883345175fd7d3a13d7",
+    "url": "/first-react-app/static/media/wall.023a46b7.bin"
+  }
+]);
